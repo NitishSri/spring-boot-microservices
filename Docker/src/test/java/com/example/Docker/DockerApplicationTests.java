3ce93cb90d24f3bf4f7b42cab6e58343f46ccc7a
AGORA\nlnitsri <nitish.srivastava@bookspot.nl>
2020-02-21 15:25:37 +0100

    Docker app test case

--- Docker/src/test/java/com/example/Docker/DockerApplicationTests.java
+++ Docker/src/test/java/com/example/Docker/DockerApplicationTests.java
@@ -1,10 +1,36 @@
 package com.example.Docker;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 class DockerApplicationTests {
+
+	@InjectMocks
+	DockerController controller;
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	public void setUp() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@Test
+	void checkStatus() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/status").accept(MediaType.TEXT_HTML_VALUE))
+				.andExpect(status().isOk()).andReturn();
+		assertEquals("Hello- world cc", mvcResult.getResponse().getContentAsString());
+	}
 
 	@Test
 	void contextLoads() {
